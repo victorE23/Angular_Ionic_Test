823df584cae5936853b27a08e10ca7b0eb08688d
Victor E. Puentes <vepuentes@dnestagency.com>
2021-02-04 10:50:57 -0500

    Saving Photos to the Filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -19,12 +19,48 @@ export class PhotoService {
       quality: 100 
     });
 
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
-    });
+      // Save the picture and add it to photo collection
+      const savedImageFile = await this.savePicture(capturedPhoto);
+      this.photos.unshift(savedImageFile);
+
   }
 
+  private async savePicture(cameraPhoto: CameraPhoto) {
+      // Convert photo to base64 format, required by Filesystem API to save
+      const base64Data = await this.readAsBase64(cameraPhoto);
+
+      // Write the file to the data directory
+      const fileName = new Date().getTime() + '.jpeg';
+      const savedFile = await Filesystem.writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: FilesystemDirectory.Data
+      });
+
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath
+      };
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;  
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 
 }
 
