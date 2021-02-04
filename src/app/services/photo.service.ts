bdb8525a9027985edb37af0630bde6b23efa688d
Victor E. Puentes <vepuentes@dnestagency.com>
2021-02-04 10:41:23 -0500

    showing photos on gallery

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -7,16 +7,28 @@ const { Camera, Filesystem, Storage } = Plugins;
   providedIn: 'root'
 })
 export class PhotoService {
-
+  public photos: Photo[] = [];
   constructor() { }
 
 
   public async addNewToGallery() {
-  // Take a photo
-  const capturedPhoto = await Camera.getPhoto({
-    resultType: CameraResultType.Uri, 
-    source: CameraSource.Camera, 
-    quality: 100 
-  });
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri, 
+      source: CameraSource.Camera, 
+      quality: 100 
+    });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
+    });
+  }
+
+
 }
+
+export interface Photo {
+  filepath: string;
+  webviewPath: string;
 }
